3c6036cf6bdc4e9d247304aad76fba433c21382a
Beaglefoot <ch-e-st@yandex.ru>
2017-01-15 23:12:05 +0300

    Add Redux state container

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
 import TestableComponent from './components/TestableComponent';
 
 require('./css/style.css');
 require('./css/style.scss');
 
-function ParentComponent() {
+const store = createStore(() => ({
+  msg: 'Redux store is created'
+}));
+
+function ParentComponent(props) {
   return (
     <div>
       <h1>freeCodeCamp React Boilerplate</h1>
       <TestableComponent />
+      <p>{props.msg}</p>
     </div>
   );
 }
 
-ReactDOM.render(<ParentComponent />, document.querySelector('.container'));
+function mapStateToProps({ msg }) {
+  return { msg };
+}
+
+const ParentContainer = connect(mapStateToProps)(ParentComponent);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ParentContainer />
+  </Provider>,
+  document.querySelector('.container')
+);
