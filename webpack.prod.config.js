ba8ae93bcb26c550d203dfa29a96864fd3a9ceda
Beaglefoot <ch-e-st@yandex.ru>
2017-02-09 20:06:34 +0300

    chore: Import settings from dev webpack config
    Some settings are duplicated and it's a pain to edit them twicely.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,46 +4,26 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 
-module.exports = {
-  entry:  './src/index.jsx',
+var moduleConfig = require('./webpack.config.js');
 
-  output: {
-    path: path.resolve(__dirname),
-    filename: 'bundle.js'
-  },
+moduleConfig.entry =  './src/index.jsx';
 
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'react-hot!babel'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.s[ac]ss$/,
-        loader: 'style!css!sass'
-      }
-    ]
-  },
-
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ],
+moduleConfig.output = {
+  path: path.resolve(__dirname),
+  filename: 'bundle.js'
 };
+
+moduleConfig.plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin()
+];
+
+module.exports = moduleConfig;
