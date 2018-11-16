58db043f8f3741afaec0e0224fa628f88768d07f
Beaglefoot <ch-e-st@yandex.ru>
2018-11-16 21:18:57 +0300

    Replace ExtractTextPlugin with MiniCssExtractPlugin
    Have to add helper function for proper config merging

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const replaceStyleLoaderWith = require('./replaceStyleLoaderWith');
 
 const {
   baseConfig,
@@ -12,70 +13,60 @@ const {
 
 cssLoaderOptions.sourceMap = false;
 
-const prodConfig = merge.smart(baseConfig, {
-  mode: 'production',
+const prodConfig = merge.smart(
+  replaceStyleLoaderWith(MiniCssExtractPlugin.loader)(/\.s[ac]ss$/, baseConfig),
+  {
+    mode: 'production',
 
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true
-      })
-    ]
-  },
-
-  output: {
-    path: path.resolve(__dirname, '..', 'dist'),
-    filename: 'bundle-[hash].js',
-    publicPath: 'dist/'
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'typings-for-css-modules-loader',
-              options: cssLoaderOptions
-            },
-            'postcss-loader',
-            'sass-loader'
-          ]
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true
         })
-      },
+      ]
+    },
 
-      {
-        test: /\.(jpe?g|png)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              optipng: {
-                optimizationLevel: 7
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              mozjpeg: {
-                progressive: true,
-                quality: 80
+    output: {
+      path: path.resolve(__dirname, '..', 'dist'),
+      filename: 'bundle-[hash].js',
+      publicPath: 'dist/'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(jpe?g|png)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                optipng: {
+                  optimizationLevel: 7
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4
+                },
+                mozjpeg: {
+                  progressive: true,
+                  quality: 80
+                }
               }
             }
-          }
-        ]
-      }
-    ]
-  },
+          ]
+        }
+      ]
+    },
 
-  plugins: [
-    new CleanWebpackPlugin(['dist'], { root: projectRootDir }),
-    new ExtractTextPlugin('styles-[md5:contenthash:hex:20].css')
-  ]
-});
+    plugins: [
+      new CleanWebpackPlugin(['dist'], { root: projectRootDir }),
+      new MiniCssExtractPlugin({
+        filename: 'styles-[contenthash].css'
+      })
+    ]
+  }
+);
 
 module.exports = prodConfig;
